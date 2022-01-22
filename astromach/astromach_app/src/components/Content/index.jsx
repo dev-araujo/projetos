ec138c767460c750f6bb5541a4f5abe2083d0b6c
dev-araujo <theriancirculo@protonmail.com>
2022-01-22 08:50:06 -0300

    delete console.log

--- astromach/astromach_app/src/components/Content/index.jsx
+++ astromach/astromach_app/src/components/Content/index.jsx
@@ -9,15 +9,6 @@ import * as S from "./styles";
 const Content = () => {
   const { isMatch } = useContext(Context);
 
-  // const preview = {
-  //   id: "2LAsTKPOVs3tavH6ga6v",
-  //   age: 26,
-  //   photo:
-  //     "https://static.ndmais.com.br/2019/07/P-15-Texto-1-Foto-1-Anitta-533x800.jpg",
-  //   name: "Anitta",
-  //   bio: "Amo cachorros e sair para dançar. Procuro alguém animado e sem neuras.",
-  // };
-  console.log(isMatch);
   return (
     <S.Container>
       {isMatch !== "" ? (
