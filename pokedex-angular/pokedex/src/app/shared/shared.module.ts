139e0e177b085574cde667a876d9e741d5a6e568
dev-araujo <theriancirculo@protonmail.com>
2022-01-25 11:54:21 -0300

    details & build

--- pokedex-angular/pokedex/src/app/shared/shared.module.ts
+++ pokedex-angular/pokedex/src/app/shared/shared.module.ts
@@ -7,17 +7,20 @@ import {RouterModule} from '@angular/router'
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
+import { PokeDetailsComponent } from './poke-details/poke-details.component';
 
 @NgModule({
   declarations: [
     PokeHeaderComponent,
     PokeSearchComponent,
-    PokeListComponent
+    PokeListComponent,
+    PokeDetailsComponent
   ],
   exports:[
     PokeHeaderComponent,
     PokeSearchComponent,
     PokeListComponent,
+    PokeDetailsComponent
   ],
   imports: [
     CommonModule,
