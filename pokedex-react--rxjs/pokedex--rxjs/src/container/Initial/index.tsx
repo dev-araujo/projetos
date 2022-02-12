dfc5b5e3af377e58eb3134ebc172f093c3e9547a
dev-araujo <theriancirculo@protonmail.com>
2022-02-12 19:16:38 -0300

    fix: search

--- pokedex-react--rxjs/pokedex--rxjs/src/container/Initial/index.tsx
+++ pokedex-react--rxjs/pokedex--rxjs/src/container/Initial/index.tsx
@@ -3,18 +3,20 @@ import { BehaviorSubject, combineLatestWith, map } from "rxjs";
 import { useObservableState } from "observable-hooks";
 import { usePokemon } from "../../store/store";
 import { go, back } from "../../utils/changePage";
-import { Search, Banner,List,FooterPaginate } from "../../components/index";
+import { Search, Banner, List, FooterPaginate } from "../../components/index";
 import Logo from "../../assets/logo.svg";
+import NotFound from "../../assets/not_found.png";
 
 import "./styles.scss";
 
 function Initial() {
   const [page, setPage] = useState({
     initial: 0,
-    end: 12
+    end: 12,
   });
   const { pokemon$ } = usePokemon();
   const search$ = useMemo(() => new BehaviorSubject(""), []);
+  const pokemon = useObservableState(pokemon$, []);
 
   const [filterPokemon] = useObservableState(
     () =>
@@ -31,10 +33,22 @@ function Initial() {
     <form className="initial">
       <Banner image={Logo} />
       <Search
-        searchValue={search$}
+        searchValue={search$.value}
         onChangeCall={(e: React.ChangeEvent<HTMLInputElement>) => search$.next(e.target.value)}
       />
-      <List pokemons={filterPokemon} start={page.initial} end={page.end} />
+      <section className="initial__list">
+        {filterPokemon.length > 0 ? (
+          <List
+            pokemons={
+              search$.value.length <= 0
+                ? filterPokemon.slice(page.initial, page.end)
+                : filterPokemon.slice(0, 150)
+            }
+          />
+        ) : (
+          <img  src={NotFound} alt={"not found"} />
+        )}
+      </section>
       <FooterPaginate
         onClickBack={(e: React.FormEvent) => back(e, setPage, page)}
         onClickGo={(e: React.FormEvent) => go(e, setPage, page)}
