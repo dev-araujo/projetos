f2c6fa9bc62d8a1c2ce2df34bb70fe58b7e5faaa
dev-araujo <theriancirculo@protonmail.com>
2022-03-09 20:31:18 -0300

    Create header

--- Angular/e-comm_movies/marketplace--angular/src/app/app.module.ts
+++ Angular/e-comm_movies/marketplace--angular/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
-
+import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PagesModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    ComponentsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent],
 })
--- Angular/e-comm_movies/marketplace--angular/src/app/pages/pages.module.ts
+++ Angular/e-comm_movies/marketplace--angular/src/app/pages/pages.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RoutingModule } from './routing.module';
+import { ComponentsModule } from '../components/components.module';
+
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RoutingModule],
+  imports: [CommonModule, RoutingModule, ComponentsModule],
 })
-
 export class PagesModule {}
