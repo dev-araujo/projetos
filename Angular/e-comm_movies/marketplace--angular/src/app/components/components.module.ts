e52e75d067675349540ab12096f8a9a44da3a9af
dev-araujo <theriancirculo@protonmail.com>
2022-03-12 13:14:12 -0300

    create home content

--- Angular/e-comm_movies/marketplace--angular/src/app/components/components.module.ts
+++ Angular/e-comm_movies/marketplace--angular/src/app/components/components.module.ts
@@ -3,15 +3,34 @@ import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentHomeComponent } from './content-home/content-home.component';
 import { NavComponent } from './nav/nav.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card'
+
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, NavComponent],
-  exports: [HeaderComponent, FooterComponent, NavComponent],
-  imports: [CommonModule, MatToolbarModule, MatSidenavModule, MatListModule],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ContentHomeComponent,
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ContentHomeComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+  ],
 })
 export class ComponentsModule {}
