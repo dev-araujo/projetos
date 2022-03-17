1de20dd762551b6d3f48509a9dbc0d82852d096f
dev-araujo <theriancirculo@protonmail.com>
2022-03-17 20:39:51 -0300

    Footer

--- React/restaurant/restaurant_app/src/components/index.js
+++ React/restaurant/restaurant_app/src/components/index.js
@@ -1,7 +1,7 @@
 import SubHeading from "./SubHeading";
-import Newsletter from "./Footer";
-import Footer from "./Footer";
+import Newsletter from "./Newsletter";
+import FooterOverlay from "./FooterOverlay";
 import MenuItem from "./Menu";
 import Navbar from "./Navbar";
 
-export { SubHeading, Newsletter, Footer, MenuItem, Navbar };
+export { SubHeading, Newsletter, FooterOverlay, MenuItem, Navbar };
